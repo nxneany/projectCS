b66d7f8be52faab303fd41e5b37f1228d5ea2d43
nxneany <65011212241@msu.ac.th>
2026-05-28 16:01:29 +0700

    up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  apiBaseUrl: 'http://localhost:3000/api',
-  // apiBaseUrl: 'https://rentalbackend-zkgn.onrender.com/api',
-  uploadsBaseUrl: 'http://localhost:3000/uploads',
-  // uploadsBaseUrl: 'https://rentalbackend-zkgn.onrender.com/uploads',
+  // apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'https://rentalbackend-zkgn.onrender.com/api',
+  // uploadsBaseUrl: 'http://localhost:3000/uploads',
+  uploadsBaseUrl: 'https://rentalbackend-zkgn.onrender.com/uploads',
   googleClientId:
     '248342611590-v8q2mvfoegvo5qgptf9pllh13g2jp0pl.apps.googleusercontent.com',
 };
